d8ec428c7c8515d99ba3c8c7c6257041b103e8e9
franciscodigilio <franciscodigilio7@gamil.com>
2024-05-30 11:18:08 -0300

    creacion

---
+++ Consulta.py
@@ -0,0 +1,18 @@
+class Consulta:
+    def __init__(self,fecha,veterinario,diagnostico,tratamiento,vacuna,ficha_medica,estado):
+        self.fecha=fecha
+        self.veterinario=veterinario
+        self.diagnostico=diagnostico
+        self.tratamiento=tratamiento
+        self.vacuna=vacuna
+        self.ficha_medica=ficha_medica
+        self.estado=estado
+
+
+    def registrar_consulta(self):
+        pass
+    def modificar_consulta(self):
+        pass
+    def consultar(self):
+        pass
+
