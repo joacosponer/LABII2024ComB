e0d7d52e529cee56dd8fbda68a30a4568f1466a8
joacosponer <joacosponer6@gmail.com>
2024-05-30 10:58:19 -0300

    creacion de clases mascota y raza

--- main.py
+++ main.py
@@ -1,41 +1,73 @@
 import persona
+from vacunas import Vacuna
+from mascota import Mascota
+from raza import Raza
 
 lista_propietarios = []
 lista_veterinarios = []
 lista_vacunas = []
+lista_mascotas = []
+lista_raza = []
 
 
 def cargar_propietarios():
-
     with open("propietarios.txt") as file:
         renglones = file.readlines()
-        for renglon in renglones:
-            datos = renglon.strip().split(",")
-            lista_propietarios.append(persona.Propietario(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]))
+    for renglon in renglones:
+        datos = renglon.strip().split(",")
+        lista_propietarios.append(persona.Propietario(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]))
 
 
 def cargar_veterinarios():
     with open("veterinarios.txt") as file:
         renglones = file.readlines()
-        for renglon in renglones:
-            datos = renglon.strip().split(",")
-            lista_veterinarios.append(persona.Veterinario(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6]))
+    for renglon in renglones:
+        datos = renglon.strip().split(",")
+        lista_veterinarios.append(persona.Veterinario(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6]))
 
 
 def cargar_Vacuna():
     with open("vacuna.txt") as file:
         lineas=file.readlines()
-        for linea in lineas:
-            datos=linea.strip().split(",")
-            lista_vacunas.append(Vacuna(datos[0], datos[1], datos[2], datos[3]))
+    for linea in lineas:
+        datos = linea.strip().split(",")
+        lista_vacunas.append(Vacuna(datos[0], datos[1], datos[2], datos[3]))
+
+
+def cargar_mascotas():
+    with open("mascotas.txt") as file:
+        lineas = file.readlines()
+    for linea in lineas:
+        datos = linea.strip().split(",")
+        obj_raza = buscar_raza(datos[4])
+        lista_mascotas.append(Mascota(datos[0], datos[1], datos[2], datos[3], obj_raza))
+
+
+def cargar_raza():
+    with open("raza.txt") as file:
+        lineas = file.readlines()
+    for linea in lineas:
+        datos = linea.strip().split(",")
+        lista_raza.append(Raza(datos[0], datos[1], datos[2], datos[3]))
+
+
+def buscar_raza(codigo):
+    for raza in lista_raza:
+        if raza.codigo == codigo:
+            return raza
+
 
 def main():
     cargar_propietarios()
     cargar_veterinarios()
     cargar_Vacuna()
+    cargar_raza()
+    cargar_mascotas()
     print(lista_propietarios)
     print(lista_veterinarios)
     print(lista_vacunas)
+    print(lista_raza)
+    print(lista_mascotas)
 
 
 if __name__ == '__main__':
---
+++ raza.py
@@ -0,0 +1,13 @@
+class Raza:
+
+    def __init__(self, codigo, nombre, especie, estado):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.especie = especie
+        self.estado = estado
+
+    def __str__(self):
+        return f"raza: {self.nombre}"
+
+    def __repr__(self):
+        return f"raza: {self.nombre}"
